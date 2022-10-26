992880d44122baafd41495928c595070548db9b1
Md.Ratul Uddin Ashraf <ratulshourov01@gmail.com>
2022-10-26 11:14:38 +0600

    state work in class component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 class Home extends Component {
-
+    constructor() {
+        super();
+        this.state={
+            name:'Md.Ratul Uddin Ashraf',
+            skill:['laravel','ajax']
+        }
+    }
     render() {
         return <div>
             <h1>{this.props.title} page</h1>
+            <h1>Name:{this.state.name}</h1>
+            <h1>Skill:{this.state.skill[1]}</h1>
             <button className="btn btn-primary" onClick={this.clickHome.bind(this,"I am the argument value")}>Click Home Page</button>
         </div>
     }
