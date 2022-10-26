8d766c1a2244a0898bcf7884df98ca0dbd1fdaa9
Md.Ratul Uddin Ashraf <ratulshourov01@gmail.com>
2022-10-26 12:53:01 +0600

    forceupdate() example using math.random()

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,16 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 class Home extends Component {
     constructor() {
         super();
+       
         this.state = {
             name: 'Md.Ratul Uddin Ashraf',
             skill: ['laravel', 'ajax'],
             project: {
                 'laravel': 'JRFUMARt'
             },
-            flag: false
+            flag: true,
         }
+        this.forceUpdateWork=this.forceUpdateWork.bind(this);
     }
     render() {
         return (
@@ -21,7 +23,9 @@ class Home extends Component {
                 <h1>project:{this.state.project.laravel}</h1>
                 <button className="btn btn-primary" onClick={this.clickHome.bind(this, "I am the argument value")}>Click Home Page</button>
                 <button className="btn btn-warning" onClick={this.changeName.bind(this, "Ratul Shourov")}>Update Name</button>
-                {this.state.flag ?<h1>True</h1> :<h1>false</h1>} 
+                {this.state.flag ? <h1>True</h1> : <h1>false</h1>}
+                <h1>Math Random{Math.random()}</h1>
+                <button className="btn btn-success" onClick={this.forceUpdateWork}>Random Value Change By Force Update </button>
             </div>)
     }
 
@@ -31,6 +35,10 @@ class Home extends Component {
     //ES6
     changeName = (updateName) => {
         this.setState({ name: updateName })
+    }
+
+    forceUpdateWork=()=>{
+        this.forceUpdate();
     }
 }
 
