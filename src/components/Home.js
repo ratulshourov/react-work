3e066417881effd8de979e24fa451c048f6232a3
Md.Ratul Uddin Ashraf <ratulshourov01@gmail.com>
2022-10-26 10:23:25 +0600

    bootstrap install and use it in the home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 class Home extends Component {
 
     render() {
         return <div>
             <h1>{this.props.title} page</h1>
-            <button onClick={this.clickHome.bind(this,"I am the argument value")}>Click Home Page</button>
+            <button className="btn btn-primary" onClick={this.clickHome.bind(this,"I am the argument value")}>Click Home Page</button>
         </div>
     }
 
