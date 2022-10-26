050836d0cdf4f9950f4c74431fd62b26a2acd079
Md.Ratul Uddin Ashraf <ratulshourov01@gmail.com>
2022-10-26 11:20:02 +0600

    state work in class component example nested object

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,10 @@ class Home extends Component {
         super();
         this.state={
             name:'Md.Ratul Uddin Ashraf',
-            skill:['laravel','ajax']
+            skill:['laravel','ajax'],
+            project:{
+                'laravel':'JRFUMARt'
+            }
         }
     }
     render() {
@@ -13,6 +16,7 @@ class Home extends Component {
             <h1>{this.props.title} page</h1>
             <h1>Name:{this.state.name}</h1>
             <h1>Skill:{this.state.skill[1]}</h1>
+            <h1>project:{this.state.project.laravel}</h1>
             <button className="btn btn-primary" onClick={this.clickHome.bind(this,"I am the argument value")}>Click Home Page</button>
         </div>
     }
