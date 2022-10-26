5e8a8c44c91ce1635c23f4dd9f018e2151434b9c
Md.Ratul Uddin Ashraf <ratulshourov01@gmail.com>
2022-10-26 11:49:35 +0600

    element variable example

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,31 +3,34 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 class Home extends Component {
     constructor() {
         super();
-        this.state={
-            name:'Md.Ratul Uddin Ashraf',
-            skill:['laravel','ajax'],
-            project:{
-                'laravel':'JRFUMARt'
-            }
+        this.state = {
+            name: 'Md.Ratul Uddin Ashraf',
+            skill: ['laravel', 'ajax'],
+            project: {
+                'laravel': 'JRFUMARt'
+            },
+            flag: false
         }
     }
     render() {
-        return <div>
-            <h1>{this.props.title} page</h1>
-            <h1>Name:{this.state.name}</h1>
-            <h1>Skill:{this.state.skill[1]}</h1>
-            <h1>project:{this.state.project.laravel}</h1>
-            <button className="btn btn-primary" onClick={this.clickHome.bind(this,"I am the argument value")}>Click Home Page</button>
-            <button className="btn btn-warning" onClick={this.changeName.bind(this,"Ratul Shourov")}>Update Name</button>
-        </div>
+        return (
+            <div>
+                <h1>{this.props.title} page</h1>
+                <h1>Name:{this.state.name}</h1>
+                <h1>Skill:{this.state.skill[1]}</h1>
+                <h1>project:{this.state.project.laravel}</h1>
+                <button className="btn btn-primary" onClick={this.clickHome.bind(this, "I am the argument value")}>Click Home Page</button>
+                <button className="btn btn-warning" onClick={this.changeName.bind(this, "Ratul Shourov")}>Update Name</button>
+                {this.state.flag ?<h1>True</h1> :<h1>false</h1>} 
+            </div>)
     }
 
     clickHome(argument) {
         alert(argument);
     }
     //ES6
-    changeName=(updateName)=>{
-        this.setState({name:updateName})
+    changeName = (updateName) => {
+        this.setState({ name: updateName })
     }
 }
 
