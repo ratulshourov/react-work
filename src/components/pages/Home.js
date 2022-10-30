ce86f9a4172fe0a82fa1e94f70cda67e11b57588
Md.Ratul Uddin Ashraf <ratulshourov01@gmail.com>
2022-10-30 15:25:29 +0600

    moduled   css

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import style from '../../Home.module.css'
 class Home extends Component {
     render() {
         const myStyle = {
@@ -9,6 +9,7 @@ class Home extends Component {
             <div>
                <h1 style={{ backgroundColor: 'red', color: 'white' }}>Home</h1> 
                 <h2 style={myStyle}>This is home page</h2>
+                <h3 className={style.TextStyleWork}>This is Moduled css</h3>
             </div>
         )
     }
