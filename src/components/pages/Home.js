e9df333d1be2e10c8e83d176273d571dd9c740d1
Md.Ratul Uddin Ashraf <ratulshourov01@gmail.com>
2022-10-30 15:15:31 +0600

    internal  css

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
 class Home extends Component {
-  render() {
-    return (
-      <div style={{backgroundColor:'red',color:'white'}}>Home</div>
-    )
-  }
+    render() {
+        const myStyle = {
+            margin:'20px'
+        }
+        return (
+            <div>
+               <h1 style={{ backgroundColor: 'red', color: 'white' }}>Home</h1> 
+                <h2 style={myStyle}>This is home page</h2>
+            </div>
+        )
+    }
 }
 export default Home
