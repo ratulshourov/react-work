c92749ed55bf4a860fa62cb2da1361f525d9f982
Md.Ratul Uddin Ashraf <ratulshourov01@gmail.com>
2022-10-30 09:36:03 +0600

    drop down and list work

--- src/components/TexAreaWork.js
+++ src/components/TexAreaWork.js
@@ -4,20 +4,47 @@ class TexAreaWork extends Component {
     constructor() {
         super();
         this.state={
-            textareaValue:''
+            textareaValue:'',
+            city1:'BD',
+            city2:'AUS',
+            selectedCity:''
+
         }
     }
   render() {
+    const country=['BD','AUS'];
+    const list=country.map((data)=>{
+        return <h1>{data}</h1>;
+    })
     return (
       <div>
         <p>TextArea :{this.state.textareaValue}</p>
-        <textarea onChange={this.handlechnage} ></textarea>
+        <textarea onChange={this.handlechnage} ></textarea><br/>
+        <p>{this.state.selectedCity}</p>
+
+        <select onChange={this.dropdownWork} value={this.state.selectedCity}>
+            <option>{this.state.city1}</option>
+            <option>{this.state.city2}</option>
+        </select>
+
+        <br/>
+        <ul>
+            {list}
+        </ul>
+
+    
+
       </div>
     )
   }
   handlechnage=(event)=>{
     var value=event.target.value;
     this.setState({textareaValue:value})
+  }
+  
+  dropdownWork=(event) =>{
+    var value=event.target.value;
+    this.setState({selectedCity:value})
   }
 }
 
