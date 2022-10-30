302a56c4d5f413e1c044d00312a65708df693075
Md.Ratul Uddin Ashraf <ratulshourov01@gmail.com>
2022-10-30 14:52:53 +0600

    basic router work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home';
-import About from './components/About';
+// import Home from './components/Home';
+// import About from './components/About';
 import ReactDomWork from './components/ReactDomWork';
 import FindDomNodeWork from './components/FindDomNodeWork';
 import FormWork from './components/FormWork';
 import TexAreaWork from './components/TexAreaWork';
 import JsonArrayListWork from './components/JsonArrayListWork';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Home from './components/pages/Home';
 function App() {
   return (
-    <div className="App">
-      <JsonArrayListWork></JsonArrayListWork>
-      {/* <TexAreaWork></TexAreaWork> */}
-      {/* <FormWork></FormWork> */}
-      {/* <FindDomNodeWork></FindDomNodeWork> */}
-      {/* <Home title="Home"></Home>
+    <Router>
+      <div >
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+        </ul>
+        <Routes>
+                 <Route exact path='/' element={< Home />}></Route>
+                 <Route exact path='/about' element={< About />}></Route>
+                 <Route exact path='/contact' element={< Contact />}></Route>
+          </Routes>
+        
+        {/* <JsonArrayListWork></JsonArrayListWork> */}
+        {/* <TexAreaWork></TexAreaWork> */}
+        {/* <FormWork></FormWork> */}
+        {/* <FindDomNodeWork></FindDomNodeWork> */}
+        {/* <Home title="Home"></Home>
       <About title="About"></About>
       <ReactDomWork></ReactDomWork> */}
-      {/* <header className="App-header">
+        {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reloads.
@@ -31,7 +59,8 @@ function App() {
           Learn React
         </a>
       </header> */}
-    </div>
+      </div>
+    </Router>
   );
 }
 
